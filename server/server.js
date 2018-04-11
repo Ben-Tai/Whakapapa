2fb7c8e54e96074420808e709cb901fcdc37a088
ben-tai <jamin.tairea@gmail.com>
2018-04-12 10:36:37 +1200

    react working

---
+++ server/server.js
@@ -0,0 +1,12 @@
+var path = require('path')
+const express = require('express')
+
+
+const server = express()
+
+
+server.use(express.json())
+server.use(express.static(path.join(__dirname, 'public')))
+
+
+module.exports = server 
